dfbdad32e55efcb926f4745142c43cf35b97b223
JohanVillard <styx.locke@gmail.com>
2024-09-02 15:21:04 +0200

    Add docstring

--- python-hello_world/2-print.py
+++ python-hello_world/2-print.py
@@ -1,3 +1,9 @@
 #!/usr/bin/python3
+"""
+A simple script to print a string to the console.
+
+This script prints the string "Programming is like
+building a multilingual puzzle" when executed.
+"""
 
 print("Programming is like building a multilingual puzzle")
