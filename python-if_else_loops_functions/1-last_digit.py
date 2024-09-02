775c631ab4fac699d03b7bd0cf09d500b3d9e4c2
JohanVillard <styx.locke@gmail.com>
2024-09-02 20:10:12 +0200

    Correct 5 output

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -13,7 +13,7 @@ if last_digit > 5:
     print(
         f"Last digit of {number} is {
             last_digit} "
-        "and is greater than 5 and not 0"
+        "and is greater than 5"
     )
 elif last_digit == 0:
     print(f"Last digit of {number} is {last_digit} and is 0")
