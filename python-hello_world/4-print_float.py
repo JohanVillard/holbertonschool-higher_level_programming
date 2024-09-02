694e25213747aca9824582cb7dc07718fe90cf29
JohanVillard <styx.locke@gmail.com>
2024-09-02 15:48:16 +0200

    Add P1T2

---
+++ python-hello_world/4-print_float.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This script prints a floating-point number formatted to two decimal places.
+
+It defines a variable `number` with a floating-point value and then prints
+the formatted string "Float: <number>" using string formatting.
+"""
+
+number = 3.14159
+print("Float: %.2f" % number)
--- python-hello_world/README.md
+++ python-hello_world/README.md
@@ -5,5 +5,10 @@ Checker: OK
 
 ## 1. Print integer
 
+Output: OK
+Checker: OK
+
+## 2.Print float
+
 Output: NOK
 Checker: NOK
