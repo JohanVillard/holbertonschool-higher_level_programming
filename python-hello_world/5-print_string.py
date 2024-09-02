ab67d92ea3fb3ae522b43afbbe24f05d4bf51a26
JohanVillard <styx.locke@gmail.com>
2024-09-02 16:11:04 +0200

    Add P1T3

---
+++ python-hello_world/5-print_string.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+This script demonstrates string manipulation and formatting.
+
+It defines a string `str` with the value "Holberton School", then:
+1. Prints the string three times in a row without spaces.
+2. Prints the first nine characters of the string.
+"""
+
+str = "Holberton School"
+
+print(f"{str}{str}{str}")
+print(f"{str[:9]}")
