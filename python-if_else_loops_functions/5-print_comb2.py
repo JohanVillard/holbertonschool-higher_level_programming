01d8b6d5fd5f71e40030a2d32b70d0087f4277eb
JohanVillard <styx.locke@gmail.com>
2024-09-03 11:38:25 +0200

    Remove docstring

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
-"""
-Prints numbers from 0 to 99, formatted with two digits, separated by commas.
 
-This script iterates through numbers from 0 to 99 and prints each number
-formatted with two digits (e.g., '00', '01', '02', ...), followed by a comma
-and a space. The last number (99) is printed without a trailing comma.
-"""
 
 for i in range(0, 100):
     print("{:02d}, ".format(i), end="")
