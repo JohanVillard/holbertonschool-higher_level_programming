2772872c9ee1c906ae319a05fb3d1654da99a902
JohanVillard <styx.locke@gmail.com>
2024-09-03 14:33:02 +0200

    Replace numeric bounds with letter bounds

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 def uppercase(str):
     upper_str = ""
     for char in str:
-        if 97 <= ord(char) <= 122:
+        if "a" <= char <= "z":
             upper_str += chr(ord(char) - 32)
         else:
             upper_str += char
