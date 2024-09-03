99527afb5eae642f522d30d6bef147d165537623
JohanVillard <styx.locke@gmail.com>
2024-09-03 10:20:20 +0200

    Use .format method and add docstring

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,12 @@
 #!/usr/bin/python3
+"""
+Prints the lowercase alphabet in a single line, excluding 'e' and 'q'.
 
-print("".join(chr(i) for i in range(97, 123) if i != 101 and i != 113), end="")
+This script iterates over the ASCII values for lowercase letters
+(from 'a' to 'z'), skips the letters 'e' (ASCII 101) and 'q' (ASCII 113),
+and prints the remaining letters on the same line without any trailing newline.
+"""
+
+for i in range(97, 123):
+    if i != 101 and i != 113:
+        print("{}".format(chr(i)), end="")
