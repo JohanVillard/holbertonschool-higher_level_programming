282094fa4397ef9cc728606803fc0f85f8249569
JohanVillard <styx.locke@gmail.com>
2024-09-03 10:26:15 +0200

    Replace by .format method and add docstring

--- python-if_else_loops_functions/4-print_hexa.py
+++ python-if_else_loops_functions/4-print_hexa.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
+"""
+Prints numbers from 0 to 98 along with their hexadecimal equivalents.
 
-print("\n".join(f"{i} = {hex(i)}" for i in range(0, 99)))
+This script iterates through numbers from 0 to 98 and prints each number
+followed by its corresponding hexadecimal value
+in the format "number = hex(number)".
+"""
+
+for i in range(0, 99):
+    print("{} = {}".format(i, hex(i)))
--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 
+
 print(", ".join(f"{i:02d}" for i in range(0, 100)), end="\n")
