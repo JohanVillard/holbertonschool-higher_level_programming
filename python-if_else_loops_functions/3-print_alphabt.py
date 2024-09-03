387eade10b8c80f1196fff2063a701dbfedf3c1f
JohanVillard <styx.locke@gmail.com>
2024-09-03 11:35:26 +0200

    Remove docstring

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,12 +1,5 @@
 #!/usr/bin/python3
-"""
-Prints the lowercase alphabet in a single line, excluding 'e' and 'q'.
-
-This script iterates over the ASCII values for lowercase letters
-(from 'a' to 'z'), skips the letters 'e' (ASCII 101) and 'q' (ASCII 113),
-and prints the remaining letters on the same line without any trailing newline.
-"""
 
 for i in range(97, 123):
     if i != 101 and i != 113:
-        print("{}".format(chr(i)), end='')
+        print("{}".format(chr(i)), end="")
