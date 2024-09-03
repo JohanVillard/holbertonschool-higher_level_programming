714472ff07fb6042abdfe107bfffccca654e4b11
JohanVillard <styx.locke@gmail.com>
2024-09-03 14:40:02 +0200

    Convert to tab

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,9 @@
 def uppercase(str):
-    up_str = ""
-    for char in str:
-        if "a" <= char <= "z":
-            up_str += chr(ord(char) - 32)
-        else:
-            up_str += char
+	up_str = ""
+	for char in str:
+		if "a" <= char <= "z":
+			up_str += chr(ord(char) - 32)
+		else:
+			up_str += char
 
-    print("{}".format(up_str))
+	print("{}".format(up_str))
