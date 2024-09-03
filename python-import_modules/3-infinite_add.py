20d47138d1404bbaa54f809bc2a9d0fdeef4f7a1
JohanVillard <styx.locke@gmail.com>
2024-09-03 17:25:32 +0200

    Iniatize result to 0 outside the number of arg check

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -15,8 +15,9 @@ from sys import argv
 if __name__ == "__main__":
     total_arg = len(argv) - 1
 
+    result = 0
+
     if total_arg != 0:
-        result = 0
         i = 1
         for arg in argv[1:]:
             result += int(arg)
