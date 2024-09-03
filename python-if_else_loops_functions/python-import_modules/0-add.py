7d15391406e96a10eb128243026abf6ea240d8bf
JohanVillard <styx.locke@gmail.com>
2024-09-03 16:00:47 +0200

    Remove docstring

--- python-if_else_loops_functions/python-import_modules/0-add.py
+++ python-if_else_loops_functions/python-import_modules/0-add.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
-"""
-Adds two numbers and prints the result.
-
-This script imports the `add` function from the module `add_0` and uses it
-to add two predefined integers, `a` and `b`. The result of the addition
-is then printed in the format "a + b = result".
-"""
 
 from add_0 import add
 
