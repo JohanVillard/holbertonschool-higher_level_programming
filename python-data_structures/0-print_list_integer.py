aebca7cd7ec28887175b81d9ed64a715b7f8d3b8
JohanVillard <styx.locke@gmail.com>
2024-09-09 09:27:45 +0200

    Add P0T0

---
+++ python-data_structures/0-print_list_integer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+
+def print_list_integer(my_list=[]):
+    """
+    Print all integers of a list.
+
+    Description:
+        This function prints all integers of a list.
+        One integer by line. We assume that the list conains only integers.
+
+    Parameters:
+        my_list:
+            is a list of integer to print.
+
+    Return:
+        None
+    """
+    for integer in my_list:
+        print("{}".format(integer))
