aec81df1bbb87449dd598a4b5b1e4bd9fb2f8d59
JohanVillard <styx.locke@gmail.com>
2024-09-09 17:28:53 +0200

    Add P1T5

---
+++ python-more_data_structures/5-number_keys.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def number_keys(a_dictionary):
+    """
+    Return the number of keys in a dictionary.
+
+    Parameters:
+    a_dictionary (dict): The dictionary from which to count the keys.
+
+    Returns:
+    int: The number of keys in the dictionary.
+    """
+    return len(a_dictionary)
