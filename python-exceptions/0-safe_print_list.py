597a3d6365be8d329ef41f0467b7077258a13a52
JohanVillard <styx.locke@gmail.com>
2024-09-10 16:19:15 +0200

    Put 'for' loop before error catcher

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -14,13 +14,15 @@ def safe_print_list(my_list=[], x=0):
         int: The number of elements successfully printed.
     """
     count = 0
-    try:
-        for num in my_list:
+    for num in my_list:
+        try:
             print("{}".format(num), end="")
             count += 1
             if x == count:
                 break
-    except IndexError:
-        pass
+        except IndexError:
+            pass
+
     print()
+
     return count
