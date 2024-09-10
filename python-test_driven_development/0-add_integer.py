ce0c0c8cbd1412052ac9ce931ffc8ebf8904773e
JohanVillard <styx.locke@gmail.com>
2024-09-10 19:38:30 +0200

    Add some line in module docstring

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,10 +1,21 @@
 #!/usr/bin/python3
-"""This module contains the function add_integer()."""
+"""
+This module contains the function `add_integer`.
+
+The `add_integer` function adds two integers, where floating-point numbers
+are converted to integers. If the provided arguments are not integers or
+floats, a `TypeError` is raised.
+
+Usage:
+    To use this module, import it and call the `add_integer` function with
+    two arguments. The function will return the sum of the two arguments
+    after converting them to integers.
+"""
 
 
 def add_integer(a, b=98):
     """
-    Add 2 integers.
+    Add 2 integers or floats.
 
     Parameters:
         a (int, float): Is the first number to add. If the input is not the
