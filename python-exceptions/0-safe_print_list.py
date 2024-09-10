d6d1bfdbd1ea3170cb13e74b9d85b8067d9fdab2
JohanVillard <styx.locke@gmail.com>
2024-09-10 16:11:57 +0200

    Fix count adding '+'

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -16,8 +16,8 @@ def safe_print_list(my_list=[], x=0):
     count = 0
     try:
         for num in my_list:
-            print("{:d}".format(num), end="")
-            count = 1
+            print("{}".format(num), end="")
+            count += 1
             if x == count:
                 break
     except IndexError:
