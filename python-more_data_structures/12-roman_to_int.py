285b00847d0d07f2e5c9ad3835d60201af93f3a0
JohanVillard <styx.locke@gmail.com>
2024-09-10 10:58:37 +0200

    Remove a None condition

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -14,7 +14,7 @@ def roman_to_int(roman_string):
     Returns:
         int: The integer value corresponding to the Roman numeral, or 0 if
              the input is not a valid Roman numeral or is not a string."""
-    if roman_string is None or isinstance(roman_string, str) == False:
+    if roman_string is None:
         return None
     # List to store arabic num style and roman key
     int_list = []
