e553b6670a7dd13c5612f2df2f3f3cf53a65d893
JohanVillard <styx.locke@gmail.com>
2024-09-10 13:09:54 +0200

    Add P2T0 and README

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+
+def safe_print_list(my_list=[], x=0):
+    """
+    Print x elements of a list.
+
+    Parameters:
+        my_list(list): can contain any type(integer, string, etc.).
+        x(int): represents the number of elements to print.
+                It can be bigger than the length of my_list.
+
+    Returns:
+        int: The number of elements successfully printed.
+    """
+    i = 0
+    try:
+        for num in my_list:
+            print(num, end="")
+            i += 1
+            if x == i:
+                break
+    except IndexError:
+        pass
+    print()
+    return i
