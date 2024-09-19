54e43502de273a50f36131cf9115ab00fd8459a5
JohanVillard <styx.locke@gmail.com>
2024-09-19 11:00:56 +0200

    Delete indentation

--- python-more_classes/9-main.py
+++ python-more_classes/9-main.py
@@ -4,11 +4,3 @@ Rectangle = __import__("9-rectangle").Rectangle
 my_square = Rectangle.square(5)
 print("Area: {} - Perimeter: {}".format(my_square.area(), my_square.perimeter()))
 print(my_square)
-
-my_square = Rectangle.square(0)
-print("Area: {} - Perimeter: {}".format(my_square.area(), my_square.perimeter()))
-print(my_square)
-
-my_square = Rectangle.square("e")
-print("Area: {} - Perimeter: {}".format(my_square.area(), my_square.perimeter()))
-print(my_square)
