7118cf762382fc47db99b9d9581e2bf130a975a8
JohanVillard <styx.locke@gmail.com>
2024-09-24 09:46:00 +0200

    Add P1T2

---
+++ python-abc/task_02_verboselist.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+"""This module defines a 'VerboseList' class."""
+
+
+class VerboseList(list):
+    """Represent a list."""
+
+    def append(self, item):
+        """
+        Add an element to the list and print a message.
+
+        Parameters:
+            item (object): The element to append in the list.
+
+        Returns:
+            None: After append, print the message  'Added [item] to the list.'
+        """
+        super().append(item)
+        print(f"Added [{item}] to the list.")
+
+    def extend(self, items):
+        """
+        Add several elements to the list and print a message.
+
+        Parameters:
+            items (object): The elements to extend in the list.
+
+        Returns:
+            None: After extend, print the message
+                  'Extended the list with [items].'
+        """
+        super().extend(items)
+        print(f"Extended the list with [{len(items)}] items.")
+
+    def remove(self, item):
+        """
+        Remove the specified element to the list and print a message.
+
+        Parameters:
+            item (object): The elements to remove from the list.
+
+        Returns:
+            None: After remove, print the message
+                  'Extended the list with [items].'
+        """
+        print(f"Removed [{item}] from the list.")
+        super().remove(item)
+
+    def pop(self, position=None):
+        """
+        Remove the element at position to the list and print a message.
+
+        Parameters:
+            position (object): The position which the element is,
+                               to it remove from the list.
+
+        Returns:
+            None: After pop, print the message
+                  'Extended the list with [items].'
+        """
+        if position is None:
+            print(f"Popped [{self[-1]}] from the list.")
+            super().pop(-1)
+        else:
+            print(f"Popped [{self[position]}] from the list.")
+            super().pop(position)
