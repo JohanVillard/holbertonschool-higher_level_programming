258fb458431d487bf9ed1ffbb3865f3f40fa1cec
JohanVillard <styx.locke@gmail.com>
2024-09-24 13:32:05 +0200

    Check parameters then initialize

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -62,7 +62,7 @@ class Rectangle(BaseGeometry):
             ValueError: If either width or height is less than or equal to 0.
         """
         super().integer_validator("width", width)
-        self.__width = width
-
         super().integer_validator("height", height)
+
+        self.__width = width
         self.__height = height
