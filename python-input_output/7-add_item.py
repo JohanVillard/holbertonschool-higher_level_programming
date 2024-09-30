32752f9b17a139218eba262196a4b5dfec253704
JohanVillard <styx.locke@gmail.com>
2024-09-30 17:11:44 +0200

    Add P0T7

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+This module defines a 'main' function that adds cmd-line arguments to a list.
+
+Saves them to a JSON file, and loads the content back from the file.
+
+This script follows these steps:
+1. Retrieves arguments from the command line (excluding the script name).
+2. Appends the arguments to a list.
+3. Saves the list to a JSON file.
+4. Loads the content from the JSON file.
+
+Functions imported:
+    load_from_json_file: Reads and parses a JSON object from a file.
+    save_to_json_file: Saves a Python object as JSON to a file.
+
+Execution:
+    The script can be executed from the command line,
+    passing any number of arguments.
+    These arguments will be added to a list and stored in 'add_item.json'.
+
+Usage example:
+    $ ./script_name.py arg1 arg2 arg3
+    This will save the list ['arg1', 'arg2', 'arg3'] to the file
+    'add_item.json'.
+"""
+
+import sys
+
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+
+if __name__ == "__main__":
+    # Name a new file
+    filename = "add_item.json"
+
+    # Add arg in a list
+    my_list = []
+
+    # Pass the command argument
+    argv = sys.argv[1:]
+    for arg in argv:
+        my_list.append(arg)
+
+    # Save list in a file
+    save_to_json_file(my_list, filename)
+
+    # Load from json file
+    load_from_json_file(filename)
