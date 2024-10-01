4b897bdd05a30e0ac7a42d6eac677b87b815441f
JohanVillard <styx.locke@gmail.com>
2024-10-01 16:09:14 +0200

    Add utf-8 encoding to convert to json

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -12,7 +12,7 @@ def convert_csv_to_json(filename):
         filename (str): The name of the CSV file to convert.
     """
     # Convert csv data into dictionary
-    with open(filename) as f_csv:
+    with open(filename, encoding="utf-8") as f_csv:
         # Check if file exists
         try:
             csv_reader = csv.DictReader(f_csv)
