35dcc8ce1847ed13fba0f7bd6c02983cd9428099
JohanVillard <styx.locke@gmail.com>
2024-10-09 17:01:23 +0200

    Add error handling decorator

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -31,15 +31,16 @@ app = Flask(__name__)
 # Instance to add authentification based on HTTP
 auth = HTTPBasicAuth()
 
-app.config["JWT_SECRET_KEY"] = (
-    """d5836d17f93e24c72c1034c5ecec92698f7c5609a181ee723c79a77b43ee2c2c06bb199c89d
-574e5209ba25e53416e2e12c1395e86c79426fb2ab2eeb1b0d8c156c48aaca783bb9649e43cac9c3a8
-0b7408b19713a14b8c12720be428636f994f39e953ab61a5ebfbd569a74f2a15b578ca95447c1bd3c3
-6f19fafb1d154b1a7eed6edcc28cd56be6e005e912cfff3c9d4a5cd75299faf776a16b375d4b736c5e
-330c69d50a8eaafb2dc14310d99df1aabfe25e82af13f3f99fcb309697ed042778120489f95c392326
-254bb34758c39eb20f7db4e2cceaccb6be96198a0d388d039b6a7786a6fc019a8d95f66e6e5ab388e6
-ee5396eb6756f860ad17fb900a2"""
-)
+app.config[
+    "JWT_SECRET_KEY"
+] = """d5836d17f93e24c72c1034c5ecec92698f7c5609a181ee723c79a77b43ee2c2c
+06bb199c89d574e5209ba25e53416e2e12c1395e86c79426fb2ab2eeb1b0d8c156c48aa
+ca783bb9649e43cac9c3a80b7408b19713a14b8c12720be428636f994f39e953ab61a5e
+bfbd569a74f2a15b578ca95447c1bd3c36f19fafb1d154b1a7eed6edcc28cd56be6e005
+e912cfff3c9d4a5cd75299faf776a16b375d4b736c5e330c69d50a8eaafb2dc14310d99
+df1aabfe25e82af13f3f99fcb309697ed042778120489f95c392326254bb34758c39eb2
+0f7db4e2cceaccb6be96198a0d388d039b6a7786a6fc019a8d95f66e6e5ab388e6ee539
+6eb6756f860ad17fb900a2"""
 jwt = JWTManager(app)
 
 
@@ -159,6 +160,62 @@ def admin_required():
 def admin_only():
     """Access to admin only."""
     return "Admin Access: Granted"
+
+
+@jwt.unauthorized_loader
+def unauthorized_callback(callback):
+    """
+    Handle error for the request without token or invalid token.
+
+    Parameters:
+        callback (str): The error message.
+
+    Return:
+        tuple: A response JSON with an error message et the status code 401.
+    """
+    return jsonify({"msg": "Missing or invalid token"}), 401
+
+
+@jwt.invalid_token_loader
+def handle_invalid_token_error(err):
+    """
+    Handle invalid JWT token errors.
+
+    Parameters:
+        err (Exception): The error that occured.
+
+    Returns:
+        tuple: A JSON response with an error message and a 401 status code.
+    """
+    return jsonify({"error": "Invalid token"}), 401
+
+
+@jwt.expired_token_loader
+def handle_expired_token_error(err):
+    """
+    Handle expired JWT token errors.
+
+    Parameters:
+        err (Exception): The error that occured.
+
+    Returns:
+        tuple: A JSON response with an error message and a 401 status code.
+    """
+    return jsonify({"error": "Token has expired"}), 401
+
+
+@jwt.needs_fresh_token_loader
+def handle_needs_fresh_token_error(err):
+    """
+    Handle errors indicating that a fresh JWT token is required.
+
+    Parameters:
+        err (Exception): The error that occured.
+
+    Returns:
+        tuple: A JSON response with an error message and a 401 status code.
+    """
+    return jsonify({"error": "Fresh token required"}), 401
 
 
 if __name__ == "__main__":
