3cddfd4e4417a0b32e430d5621953edd98ccef05
JohanVillard <styx.locke@gmail.com>
2024-10-25 13:26:03 +0200

    Fix bad relationships

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,8 +1,13 @@
 -- Create a MySQL server user user_0d_1
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
 
 -- Give all privileges of this server
-GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES 
+ON *.*
+TO 'user_0d_1'@'localhost';
 
 -- Ensure that privileges takes effect
 FLUSH PRIVILEGES;
+
+
