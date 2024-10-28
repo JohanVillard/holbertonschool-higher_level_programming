04ec611a2e7efa262b2629826e504d89ceba30d4
JohanVillard <styx.locke@gmail.com>
2024-10-28 09:22:42 +0100

    Set charset into engine into latin1

--- python-object_relational_mapping/6-model_state.py
+++ python-object_relational_mapping/6-model_state.py
@@ -7,7 +7,7 @@ from sqlalchemy import create_engine
 
 if __name__ == "__main__":
     engine = create_engine(
-        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}?charset=latin1".format(
             sys.argv[1], sys.argv[2], sys.argv[3]
         ),
         pool_pre_ping=True,
