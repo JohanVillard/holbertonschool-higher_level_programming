53b42ba0681a4a4931b455056849ff063673b1b9
JohanVillard <styx.locke@gmail.com>
2024-11-06 22:26:08 +0100

    Add pycodestyle and module docstring

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -1,3 +1,5 @@
+"""This module defines a function that generates invitations."""
+
 import logging
 import os
 
@@ -29,19 +31,19 @@ def generate_invitations(template, attendees):
 
             template_to_write = template
             for key, value in attendee.items():
-                key_without_brace = f"{{{key}}}"
-                template_to_write = template_to_write.replace(key_without_brace, value)
+                form_key = f"{{{key}}}"
+                template_to_write = template_to_write.replace(form_key, value)
 
             while os.path.exists(f"output_{i}.txt"):
                 print(f"output_{i}.txt already exists.")
                 i += 1
 
             try:
-                with open(f"output_{i}.txt", "w", encoding="utf-8") as f:
+                with open(f"output_{i}.txt", "x", encoding="utf-8") as f:
                     f.write(template_to_write)
                     i += 1
             except Exception as e:
-                logging.error(f"The file output_{i}.txt could not be created : {e}")
+                logging.error(f"output_{i}.txt could not be created : {e}")
     except Exception as e:
         logging.error(e)
 
