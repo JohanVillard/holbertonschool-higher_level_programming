4959f32cf3842cacd76a42ba1a25ad5d46d344cd
JohanVillard <styx.locke@gmail.com>
2024-11-08 20:01:39 +0100

    test

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -8,22 +8,22 @@ with open("template.txt", "r") as f:
 
 def generate_invitations(template, attendees):
     """Generate invitation."""
-    try:
-        if not isinstance(template, str):
-            raise TypeError(f"Template have invalid type: {type(template)}")
+    if not isinstance(template, str):
+        raise TypeError(f"Template have invalid type: {type(template)}")
 
-        if not is_list_of_dict(attendees):
-            raise TypeError(f"Attendees have invalid type: {type(attendees)}")
+    if not is_list_of_dict(attendees):
+        raise TypeError(f"Attendees have invalid type: {type(attendees)}")
 
-        if not template:
-            raise ValueError("Template is empty, no output files generated.")
+    if not template:
+        raise ValueError("Template is empty, no output files generated.")
 
-        if not attendees:
-            raise ValueError("No data provided, no output files generated.")
+    if not attendees:
+        raise ValueError("No data provided, no output files generated.")
 
-        # File incrementer
-        i = 1
-        for attendee in attendees:
+    # File incrementer
+    i = 1
+    for attendee in attendees:
+        try:
             for key, value in attendee.items():
                 if value is None:
                     attendee[key] = "N/A"
