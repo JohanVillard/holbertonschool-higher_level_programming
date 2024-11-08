cfd5f551eb49daa291cdd8dd965d576e7dd298e5
JohanVillard <styx.locke@gmail.com>
2024-11-08 20:06:48 +0100

    Test

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -22,30 +22,33 @@ def generate_invitations(template, attendees):
 
     # File incrementer
     i = 1
-    for attendee in attendees:
-        try:
-            for key, value in attendee.items():
-                if value is None:
-                    attendee[key] = "N/A"
-
-            template_to_write = template
-            for key, value in attendee.items():
-                form_key = f"{{{key}}}"
-                template_to_write = template_to_write.replace(form_key, value)
-
-            while os.path.exists(f"output_{i}.txt"):
-                print(f"output_{i}.txt already exists.")
-                i += 1
-
+    try:
+        for attendee in attendees:
             try:
-                with open(f"output_{i}.txt", "x", encoding="utf-8") as f:
-                    f.write(template_to_write)
-                    i += 1
-            except Exception as e:
-                print(f"output_{i}.txt could not be created : {e}")
+                for key, value in attendee.items():
+                    if value is None:
+                        attendee[key] = "N/A"
 
-        except Exception as e:
-            print(f"An error occured: {e}")
+                template_to_write = template
+                for key, value in attendee.items():
+                    form_key = f"{{{key}}}"
+                    template_to_write = template_to_write.replace(form_key, value)
+
+                while os.path.exists(f"output_{i}.txt"):
+                    print(f"output_{i}.txt already exists.")
+                    i += 1
+
+                try:
+                    with open(f"output_{i}.txt", "x", encoding="utf-8") as f:
+                        f.write(template_to_write)
+                        i += 1
+                except Exception as e:
+                    print(f"output_{i}.txt could not be created : {e}")
+
+            except Exception as e:
+                print(f"An error occured: {e}")
+    except Exception as e:
+        print(f"An error occured: {e}")
 
 
 def is_list_of_dict(attendees):
