318b6e93c9407e9a3e323a85cf95449feae66255
JohanVillard <styx.locke@gmail.com>
2024-11-08 19:47:21 +0100

    test

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -1,6 +1,7 @@
 """This module defines a function that generates invitations."""
 
 import logging
+import os
 
 with open("template.txt", "r") as f:
     template = f.read()
@@ -8,19 +9,19 @@ with open("template.txt", "r") as f:
 
 def generate_invitations(template, attendees):
     """Generate invitation."""
+    if not isinstance(template, str):
+        raise TypeError(f"Template have invalid type: {type(template)}")
+
+    if not is_list_of_dict(attendees):
+        raise TypeError(f"Attendees have invalid type: {type(attendees)}")
+
+    if not template:
+        raise ValueError("Template is empty, no output files generated.")
+
+    if not attendees:
+        raise ValueError("No data provided, no output files generated.")
+
     try:
-        if not isinstance(template, str):
-            raise TypeError(f"Template have invalid type: {type(template)}")
-
-        if not is_list_of_dict(attendees):
-            raise TypeError(f"Attendees have invalid type: {type(attendees)}")
-
-        if not template:
-            raise ValueError("Template is empty, no output files generated.")
-
-        if not attendees:
-            raise ValueError("No data provided, no output files generated.")
-
         # File incrementer
         i = 1
         for attendee in attendees:
@@ -33,12 +34,18 @@ def generate_invitations(template, attendees):
                 form_key = f"{{{key}}}"
                 template_to_write = template_to_write.replace(form_key, value)
 
-            with open(f"output_{i}.txt", "x", encoding="utf-8") as f:
-                f.write(template_to_write)
-            i += 1
+            while os.path.exists(f"output_{i}.txt"):
+                print(f"output_{i}.txt already exists.")
+                i += 1
 
+            try:
+                with open(f"output_{i}.txt", "x", encoding="utf-8") as f:
+                    f.write(template_to_write)
+                    i += 1
+            except Exception as e:
+                logging.error(f"output_{i}.txt could not be created : {e}")
     except Exception as e:
-        logging.error(f"output_{i}.txt could not be created : {e}")
+        logging.error(e)
 
 
 def is_list_of_dict(attendees):
