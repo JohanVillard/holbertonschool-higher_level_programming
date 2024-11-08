63cad444683950059802eb96513c7dc58798c05d
JohanVillard <styx.locke@gmail.com>
2024-11-08 19:31:02 +0100

    Test

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -19,8 +19,8 @@ def generate_invitations(template, attendees):
 
     # File incrementer
     i = 1
-    try:
-        for attendee in attendees:
+    for attendee in attendees:
+        try:
             template_to_write = template
             for key, value in attendee.items():
                 template_to_write = template_to_write.replace(
@@ -35,9 +35,9 @@ def generate_invitations(template, attendees):
                 template_to_write = template_to_write.replace(
                     "{event_location}", attendee.get("event_location") or "N/A"
                 )
-            if os.path.exists(f"output_{i}.txt"):
+            while os.path.exists(f"output_{i}.txt"):
                 print(f"output_{i}.txt already exists.")
-                continue
+                i += 1
 
             with open(f"output_{i}.txt", "w", encoding="utf-8") as f:
                 f.write(template_to_write)
@@ -45,8 +45,8 @@ def generate_invitations(template, attendees):
 
             i += 1
 
-    except Exception as e:
-        print(f"output_{i}.txt could not be created : {e}")
+        except Exception as e:
+            print(f"output_{i}.txt could not be created : {e}")
 
 
 def is_list_of_dict(attendees):
