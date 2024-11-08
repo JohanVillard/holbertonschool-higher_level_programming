9b256eed54e7086c8cedf860732e283e5474e344
JohanVillard <styx.locke@gmail.com>
2024-11-08 19:43:28 +0100

    Rewind

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -1,47 +1,51 @@
 """This module defines a function that generates invitations."""
 
+import logging
 import os
+
+with open("template.txt", "r") as f:
+    template = f.read()
 
 
 def generate_invitations(template, attendees):
-    """Generate invitation with a template."""
-    if not isinstance(template, str):
-        raise TypeError(f"Template have invalid type: {type(template)}")
+    """Generate invitation."""
+    try:
+        if not isinstance(template, str):
+            raise TypeError(f"Template have invalid type: {type(template)}")
 
-    if not is_list_of_dict(attendees):
-        raise TypeError(f"Attendees have invalid type: {type(attendees)}")
+        if not is_list_of_dict(attendees):
+            raise TypeError(f"Attendees have invalid type: {type(attendees)}")
 
-    if not template.strip():
-        raise ValueError("Template is empty, no output files generated.")
+        if not template:
+            raise ValueError("Template is empty, no output files generated.")
 
-    if not attendees:
-        raise ValueError("No data provided, no output files generated.")
+        if not attendees:
+            raise ValueError("No data provided, no output files generated.")
 
-    # File incrementer
-    for index, attendee in enumerate(attendees, start=1):
-        try:
+        # File incrementer
+        i = 1
+        for attendee in attendees:
+            for key, value in attendee.items():
+                if value is None:
+                    attendee[key] = "N/A"
+
             template_to_write = template
-            template_to_write = template_to_write.replace(
-                "{name}", attendee.get("name") or "N/A"
-            )
-            template_to_write = template_to_write.replace(
-                "{event_title}", attendee.get("event_title") or "N/A"
-            )
-            template_to_write = template_to_write.replace(
-                "{event_date}", attendee.get("event_date") or "N/A"
-            )
-            template_to_write = template_to_write.replace(
-                "{event_location}", attendee.get("event_location") or "N/A"
-            )
-            while os.path.exists(f"output_{index}.txt"):
-                index += 1
+            for key, value in attendee.items():
+                form_key = f"{{{key}}}"
+                template_to_write = template_to_write.replace(form_key, value)
 
-            with open(f"output_{index}.txt", "w", encoding="utf-8") as f:
-                f.write(template_to_write)
-                print(f"output__{index}.txt created.")
+            while os.path.exists(f"output_{i}.txt"):
+                print(f"output_{i}.txt already exists.")
+                i += 1
 
-        except Exception as e:
-            print(f"output_{index}.txt could not be created : {e}")
+            try:
+                with open(f"output_{i}.txt", "x", encoding="utf-8") as f:
+                    f.write(template_to_write)
+                    i += 1
+            except Exception as e:
+                logging.error(f"output_{i}.txt could not be created : {e}")
+    except Exception as e:
+        logging.error(e)
 
 
 def is_list_of_dict(attendees):
