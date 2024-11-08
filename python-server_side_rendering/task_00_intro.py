f4dd066f1484e4c8eafda940d9b88d48ec04ec7a
JohanVillard <styx.locke@gmail.com>
2024-11-08 19:09:38 +0100

    Rework error handling

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -5,22 +5,22 @@ import os
 
 def generate_invitations(template, attendees):
     """Generate invitation with a template."""
+    if not isinstance(template, str):
+        raise TypeError(f"Template have invalid type: {type(template)}")
+
+    if not is_list_of_dict(attendees):
+        raise TypeError(f"Attendees have invalid type: {type(attendees)}")
+
+    # Delete the whitespaces and check if there is a string
+    if not template.strip():
+        raise ValueError("Template is empty, no output files generated.")
+
+    if not attendees:
+        raise ValueError("No data provided, no output files generated.")
+
+    # File incrementer
+    i = 1
     try:
-        if not isinstance(template, str):
-            raise TypeError(f"Template have invalid type: {type(template)}")
-
-        if not is_list_of_dict(attendees):
-            raise TypeError(f"Attendees have invalid type: {type(attendees)}")
-
-        # Delete the whitespaces and check if there is a string
-        if not template.strip():
-            raise ValueError("Template is empty, no output files generated.")
-
-        if not attendees:
-            raise ValueError("No data provided, no output files generated.")
-
-        # File incrementer
-        i = 1
         for attendee in attendees:
             template_to_write = template
             for key, value in attendee.items():
@@ -33,15 +33,13 @@ def generate_invitations(template, attendees):
                 print(f"output_{i}.txt already exists.")
                 i += 1
 
-            try:
-                with open(f"output_{i}.txt", "x", encoding="utf-8") as f:
-                    f.write(template_to_write)
-                    print(f"output__{i}.txt created.")
-                i += 1
-            except Exception as e:
-                print(f"output_{i}.txt could not be created : {e}")
+            with open(f"output_{i}.txt", "x", encoding="utf-8") as f:
+                f.write(template_to_write)
+                print(f"output__{i}.txt created.")
+            i += 1
+
     except Exception as e:
-        print(e)
+        print(f"output_{i}.txt could not be created : {e}")
 
 
 def is_list_of_dict(attendees):
